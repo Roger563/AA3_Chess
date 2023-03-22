42cb5302833a380adb43c276804c945c955486ae
Roger Aguilar <roger.aguilar@enti.cat>
2023-03-22 09:15:09 +0100

    set up

---
+++ ChessClient/chessGame_v2/GameClient/main.cpp
@@ -0,0 +1,11 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <fstream>
+#include "ChessBoard.h"
+using namespace sf;
+int main(){
+	ChessBoard graphics;
+	graphics.run();
+	return 0;
+}
